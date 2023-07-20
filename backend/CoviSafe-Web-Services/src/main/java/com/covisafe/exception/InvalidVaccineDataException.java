d8198c5a749caab7b934b8a39419a401016b5a44
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-07-21 04:29:04 +0530

    member-user-crud-operation-with-some-minor-rectification

--- backend/CoviSafe-Web-Services/src/main/java/com/covisafe/exception/InvalidVaccineDataException.java
+++
@@ -1,8 +0,0 @@
-package com.covisafe.exception;
-
-public class InvalidVaccineDataException extends Exception {
-
-    public InvalidVaccineDataException(String message) {
-        super(message);
-    }
-}
