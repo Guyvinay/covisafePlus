c6689fdafdbd795198fd91bb3235302599d17d30
Shashanksarojj <shashankdw1704@gmail.com>
2023-07-21 21:02:59 +0530

    day-4

---
+++ backend/CoviSafe-Web-Services/src/main/java/com/covisafe/config/SecurityConstants.java
@@ -0,0 +1,10 @@
+package com.covisafe.config;
+
+public interface SecurityConstants {
+	
+	
+	public static final String JWT_KEY ="ThisIsTheSecretKeyForAuthenticationOfVaccine";
+	public static final String JWT_HEADER = "Authorization";
+
+
+}
