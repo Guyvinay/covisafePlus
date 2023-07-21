1a7b75bbfca01a4586ab88a8d5acb747d0ba0f55
Rupesh0913 <k.rupeshsinha0913@gmail.com>
2023-07-21 20:52:33 +0530

    Service & Implementation

--- backend/CoviSafe-Web-Services/src/main/java/com/covisafe/service/AppointmentService.java
+++ backend/CoviSafe-Web-Services/src/main/java/com/covisafe/service/AppointmentService.java
@@ -1,5 +1,13 @@
 package com.covisafe.service;
 
-public interface AppointmentService {
+import java.util.List;
 
+import com.covisafe.modal.Appointment;
+
+public interface AppointmentService {
+	public List<Appointment> getAllAppointment();
+	public Appointment getAppointment(Long bookingId);
+	public Appointment addAppointment(Appointment app);
+	public Appointment updateAppointment(Appointment app);
+	public Boolean deleteAppointment(Appointment app);
 }
--- backend/CoviSafe-Web-Services/src/main/java/com/covisafe/service/VaccinationCenterService.java
+++ backend/CoviSafe-Web-Services/src/main/java/com/covisafe/service/VaccinationCenterService.java
@@ -1,5 +1,14 @@
 package com.covisafe.service;
 
-public interface VaccinationCenterService {
+import java.util.List;
+import com.covisafe.modal.VaccinationCenter;
 
+public interface VaccinationCenterService {
+	
+	public List<VaccinationCenter> getAllVaccinationCenter();
+	public VaccinationCenter getVaccination(int centerid);
+	public VaccinationCenter addVaccinationCenter(VaccinationCenter center);
+	public VaccinationCenter updateVaccineCenter(VaccinationCenter center);
+	public Boolean deleteVaccinationCenter(VaccinationCenter center);
+	
 }
