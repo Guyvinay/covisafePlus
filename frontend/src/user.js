019da907b1e8c524f16f4a0faf37ba90105649db
anuragjatofficial <anurag.1101140@gmail.com>
2023-07-23 04:06:18 +0530

    added update member filed

--- frontend/src/user.js
+++ frontend/src/user.js
@@ -1,4 +1,6 @@
-let URL = `http://localhost:8888/`;
+let URL = `http://localhost:8888/`; // base server URL 
+
+//object of appointment type
 class Appointment{
     constructor(name,slot,status,userId){
         this.name = name;
@@ -7,6 +9,18 @@ class Appointment{
         this.userId = userId;
     }
 }
+
+// object of update user type
+
+class UpdateUser{
+    constructor(id,name){
+        this.id = id;
+        this.name = name;
+    }
+}
+
+
+// evenetlistner for add appointment form 
 
 document
 .getElementById("add-employee")
@@ -28,22 +42,27 @@ document
     const userId = document
                     .getElementById("member-id")
                     .value;
-    let appointment = new Appointment(name,
-                                      slot,
-                                      status,
-                                      userId
-                                     );
+    if(name && slot && status){
+        let appointment = new Appointment(name,
+            slot,
+            status,
+            userId
+        );
 
-    RequestServer(appointment)
-    .then((data)=>{
-        console.log(data);
-    })
-    .catch((err)=>{
-        alert("can't fetch data");
-        console.error(err);
-    })
+        RequestServer(appointment)
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((err) => {
+                alert("can't fetch data");
+                console.error(err);
+            })
+    }else{
+        alert("please fill the complete details");
+    }
 })
 
+// function resposible to add an appointment in server
 
 function RequestServer(appointment){
    return fetch(URL +`/appointments/${appointment.userId}/1`,
@@ -54,6 +73,52 @@ function RequestServer(appointment){
             'Content-Type' : 'application/json'
         },
         body : JSON.stringify(appointment)
+    })
+    .then(res=>res.json());
+}
+
+/**
+ * code for update member starts here
+ * 
+ */
+
+document
+    .getElementById("update-employee")
+    .addEventListener("click",
+    ()=>{
+        const id = document
+                    .getElementById("update-member-id");
+        
+        const name = document
+                        .getElementById("update-member-name");
+
+        if(id && name ){
+            let updateUser = new UpdateUser(id, name);
+            UpdateUserFuntion(updateUser)
+            .then((res)=>{
+                console.log(res);
+            })
+            .catch((res)=>{
+                console.error(res);
+                alert("can't update for now ");
+            });
+        }else{
+            alert("please fill complete details");
+        }
+            
+    })
+
+    // funcion to update user
+
+function UpdateUserFuntion(updateUser){
+    return fetch(URL + `/appointments/${updateUser.id}`,
+    {
+        method : 'PUT',
+        headers : 
+        {
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify(updateUser);
     })
     .then(res=>res.json());
 }
