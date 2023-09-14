ea5b12f548113dd2492384e161f716cd8cf2838f
anuragjatofficial <anurag.1101140@gmail.com>
2023-09-15 01:00:03 +0530

    replaced all user to idcard

--- backend/CoviSafe-Web-Services/src/main/java/com/covisafe/repository/IdCardRepository.java
+++ backend/CoviSafe-Web-Services/src/main/java/com/covisafe/repository/IdCardRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.covisafe.modal.IdCard;
-import com.covisafe.modal.User;
 
 public interface IdCardRepository extends JpaRepository<IdCard, Integer> {
 
