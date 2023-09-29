801d87667f4b03363f55b8c14d6f3476ea4d5ef0
anuragjatofficial <anurag.1101140@gmail.com>
2023-09-29 16:58:43 +0530

    updated profile.jsx

--- frontend/covisafe/src/components/Dashboard.jsx
+++ frontend/covisafe/src/components/Dashboard.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Footer from './Footer';
 import Nav from './Nav';
+import Profile from './Profile'
 
 export default function Dashboard({ zoom: [zoom, setZoom] }) {
   return (
     <>
       <Nav zoom={[zoom, setZoom]} />
+      <div className="w-full" style={{ minHeight: "87vh" }}>
+        {/* <Profile /> */}
+      </div>
       <Footer />
     </>
   );
