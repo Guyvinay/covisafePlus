b16081e23ea79255b98b61403e9252d0e0e08ba5
anuragjatofficial <anurag.1101140@gmail.com>
2023-10-04 21:55:52 +0530

    added basic structure

--- frontend/covisafe-admin/src/App.tsx
+++ frontend/covisafe-admin/src/App.tsx
@@ -1,15 +1,49 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css'
+import { Box } from "@chakra-ui/layout";
+import * as React from "react";
+import { useState } from "react";
 
-function App() {
+export const App = () => {
 
   return (
-    <>
-      
-    </>
-  )
-}
-
-export default App;
+    <Box
+      display={"flex"}
+      justifyContent={"space-around"}
+      width={"100%"}
+      flexDir={"row"}
+      shadow={"outline"}
+      paddingTop={"12px"}
+      paddingBottom={"12px"}
+      boxShadow="sm"
+    >
+      <div style={{ width: "10%" }}>
+        <a
+          href=""
+          className="font-medium px-6 text-2xl"
+          style={{ fontFamily: "Ubuntu, sans-serif" }}
+        >
+          Lo<span className="text-red-600">go</span>
+        </a>
+      </div>
+      <ul
+        className="flex justify-around text-red-600 font-semibold hover:"
+        style={{ width: "90%" }}
+      >
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="" className="after:w-9 after:h-2 after:bg-red-500">About</a>
+          <div></div>
+        </li>
+        <li>
+          <a href="">Dashboard</a>
+        </li>
+        <li>
+          <a href="">Users</a>
+        </li>
+      </ul>
+    </Box>
+  );
+};
--- frontend/covisafe-admin/tailwind.config.js
+++ frontend/covisafe-admin/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js,ts}"],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
